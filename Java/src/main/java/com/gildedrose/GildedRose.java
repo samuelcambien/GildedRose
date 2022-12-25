b69447805b41608b7a4a41a5dfb9de4547a83506
samuel <samuel@atmire.com>
2022-12-25 20:50:24 +0100

    delete itemTypes map

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,10 +1,5 @@
 package com.gildedrose;
 
-import static java.util.function.Function.identity;
-import static java.util.stream.Collectors.toMap;
-
-import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Stream;
 
 class GildedRose {
@@ -12,20 +7,19 @@ class GildedRose {
     public static final QualityUpdater DEFAULT_QUALITY_UPDATER = item -> item.sellIn > 0 ? -1 : -2;
 
     Item[] items;
-    private final Map<String, ItemType> itemTypes;
 
     public GildedRose(Item[] items) {
         this.items = items;
-        this.itemTypes = Stream.of(ItemType.values())
-                               .collect(toMap(ItemType::getName, identity()));
     }
 
     public void updateQuality() {
         for (Item item : items) {
-            Optional.ofNullable(itemTypes.get(item.name))
-                    .map(ItemType::getQualityUpdater)
-                    .orElse(DEFAULT_QUALITY_UPDATER)
-                    .accept(item);
+            Stream.of(ItemType.values())
+                  .filter(itemType -> itemType.getName().equals(item.name))
+                  .map(ItemType::getQualityUpdater)
+                  .findAny()
+                  .orElse(DEFAULT_QUALITY_UPDATER)
+                  .accept(item);
         }
     }
 }
