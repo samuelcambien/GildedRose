f9dc4f92fd813f5d9cd2ad39c312fb86f933f376
samuel <samuel@atmire.com>
2023-01-18 15:56:39 +0100

    repair code behaviour for wrong usage

--- Java/src/main/java/com/gildedrose/QualityUpdater.java
+++ Java/src/main/java/com/gildedrose/QualityUpdater.java
@@ -8,8 +8,11 @@ import java.util.function.Consumer;
 /**
  * This Consumer will:
  * - first update the quality of a given item by adding the amount given by
- * {@link QualityUpdater#getAmount(Item)} to the existing quality. This amount can be negative. The resulting
- * quality will always be capped at {@link QualityUpdater#MAX_QUALITY} and will never be negative.
+ * {@link QualityUpdater#getAmount(Item)} to the existing quality. This amount can be negative.
+ *   - for increasing quality (positive amount), the resulting quality will be capped at {@link QualityUpdater#MAX_QUALITY},
+ *     unless the initial quality is already above this limit, in that case it won't be changed
+ *   - for decreasing quality (negative amount), the resulting quality will not become negative,
+ *     unless the initial quality is already negative, in that case it won't be changed
  * - then decrease the sellin property of the item by 1.
  */
 @FunctionalInterface
@@ -19,7 +22,13 @@ public interface QualityUpdater extends Consumer<Item> {
 
     @Override
     default void accept(Item item) {
-        item.quality = max(0, min(MAX_QUALITY, item.quality + getAmount(item)));
+        int amount = getAmount(item);
+        if (amount > 0 && item.quality < MAX_QUALITY) {
+            item.quality = min(MAX_QUALITY, item.quality + amount);
+        }
+        if (amount < 0 && item.quality > 0) {
+            item.quality = max(0, item.quality + amount);
+        }
         item.sellIn--;
     }
 
